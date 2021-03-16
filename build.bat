a97b490114276ec3056e3210d4ed6cd8a24d2e04
Jovan Ristic <jristic42@gmail.com>
2021-03-16 15:34:01 -0700

    Populating with imgui win32 dx11 example.

--- build.bat
+++ build.bat
@@ -2,17 +2,14 @@
 
 call project.bat
 
-set BuildFolder=build
+set BuildFolder=built
 
-set VulkanIncludePath="%VK_SDK_PATH%\Include"
-set VulkanLibPath="%VK_SDK_PATH%\Lib"
-set SdlIncludePath="SDL\include"
-set SdlLibPath="SDL\lib\x64"
-set CommonCompilerFlags=/MTd /nologo /fp:fast /Gm- /GR- /EHa- /Od /Oi /WX /W4 /wd4201 /FC /Z7 /D_CRT_SECURE_NO_WARNINGS /Iimgui /I%VulkanIncludePath% /I%SdlIncludePath% /Fo%BuildFolder%\
-set CommonLinkerFlags=/incremental:no /opt:ref /subsystem:console /libpath:%VulkanLibPath% /libpath:%SdlLibPath% vulkan-1.lib sdl2.lib
+set ImguiPath="external\imgui"
+set CommonCompilerFlags=/MTd /nologo /fp:fast /Gm- /GR- /EHa- /Od /Oi /WX /W4 /wd4201 /FC /Z7 /D_CRT_SECURE_NO_WARNINGS /Iimgui /Fo%BuildFolder%\
+set CommonLinkerFlags=/incremental:no /opt:ref /subsystem:console d3d11.lib d3dcompiler.lib
 
-set AdditionalSourceFiles=imgui/imgui.cpp imgui/imgui_draw.cpp imgui/imgui_demo.cpp imgui/imgui_widgets.cpp source/imgui_impl_sdl.cpp source/imgui_impl_vulkan.cpp
+set AdditionalSourceFiles=imgui/imgui.cpp imgui/imgui_draw.cpp imgui/imgui_demo.cpp imgui/imgui_tables.cpp imgui/imgui_widgets.cpp source/imgui_impl_win32.cpp source/imgui_impl_dx11.cpp
 
 if not exist %BuildFolder%\ mkdir %BuildFolder%
 
-cl.exe %CommonCompilerFlags% source/win32_main.cpp %AdditionalSourceFiles% /Fe%ProjectExe% /link %CommonLinkerFlags%
+cl.exe %CommonCompilerFlags% source/win32_main.cpp %AdditionalSourceFiles% /Fe%BuildFolder%/%ProjectExe% /link %CommonLinkerFlags%
