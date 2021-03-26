b120a1fe21597507d86081101b3749feff67ee03
Jovan Ristic <jristic42@gmail.com>
2021-03-25 22:05:52 -0700

    More shader fun.

--- source/shader.hlsl
+++ source/shader.hlsl
@@ -17,8 +17,17 @@ void CSMain(uint3 DTid : SV_DispatchThreadID)
 	float2 delta = DTid.xy - TextureSize/2;
 	float2 deltaSqr = delta*delta;
 	float dist = sqrt(deltaSqr.x + deltaSqr.y);
-	float intensity = (sin(dist/30 - Time*10) + 1) /2;
+
+	float theta;
+	if (delta.y > 0)
+		theta = 0.5 * atan2(delta.y,delta.x) /3.14;
+	else
+		theta = (0.5*atan2(-delta.y,-delta.x) )/3.14 + 0.5;
+
+	float intensity = (sin(dist/30 + (theta)*3.14*2 - Time*10) + 1) /2;
 	if ((DTid.x/4 + DTid.y/4)%2 == 0)
 		intensity = pow(intensity,2);
+
+	
 	OutTexture[DTid.xy] = float4(intensity, intensity, intensity, 1);
 }
