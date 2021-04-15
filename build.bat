b6ba131b9c9e3d582d3ef736b77080bc5f05ed0a
Jovan Ristic <jristic42@gmail.com>
2021-04-15 16:18:11 -0700

    Setting up build script for release/debug configs.

--- build.bat
+++ build.bat
@@ -3,11 +3,23 @@
 call project.bat
 
 set BuildFolder=built
+set ExternalPath=external
 
-set ExternalPath="external"
-set CommonCompilerFlags=/MTd /nologo /fp:fast /Gm- /GR- /EHa- /Od /Oi /WX /W4 /wd4201 /FC /Z7 /D_CRT_SECURE_NO_WARNINGS /D_HAS_EXCEPTIONS=0 /I%ExternalPath% /Fo%BuildFolder%\
-set CommonLinkerFlags=/incremental:no /opt:ref /subsystem:windows d3d11.lib d3dcompiler.lib dxguid.lib
+if /i "%1"=="release" (
+	set Config=release
+	set ConfigCompilerOptions=/MT /O2 /Oi /Oy /GL
+	set ConfigLinkerOptions=/opt:ref
+) else (
+	set Config=debug
+	set ConfigCompilerOptions=/MTd /Od
+	set ConfigLinkerOptions=/opt:noref /debug
+)
+
+set CommonCompilerFlags=%ConfigCompilerOptions% /nologo /fp:fast /Gm- /GR- /EHa- /WX /W4 /wd4201 /FC /Z7 /D_CRT_SECURE_NO_WARNINGS /D_HAS_EXCEPTIONS=0 /I%ExternalPath% /Fo%BuildFolder%\
+set CommonLinkerFlags=%ConfigLinkerOptions% /incremental:no /subsystem:windows d3d11.lib d3dcompiler.lib dxguid.lib
 
 if not exist %BuildFolder%\ mkdir %BuildFolder%
+
+echo Compiling: renderland.exe, Config: %Config%
 
 cl.exe %CommonCompilerFlags% source/win32_main.cpp /Fe%BuildFolder%/%ProjectExe% /link %CommonLinkerFlags%
