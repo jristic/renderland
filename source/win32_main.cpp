61e21f58750e48d83d63b87ab3c49afd960209af
Jovan Ristic <jristic42@gmail.com>
2021-05-10 17:03:40 -0700

    A mystery.

--- source/win32_main.cpp
+++ source/win32_main.cpp
@@ -278,7 +278,13 @@ void CreateRenderTarget()
 {
 	ID3D11Texture2D* pBackBuffer;
 	g_pSwapChain->GetBuffer(0, IID_PPV_ARGS(&pBackBuffer));
-	g_pd3dDevice->CreateRenderTargetView(pBackBuffer, NULL, &g_mainRenderTargetView);
+	// TODO: Should we not be being using an SRGB conversion on the backbuffer? 
+	//	From my understanding we should, but it looks visually wrong to me. 
+	// D3D11_RENDER_TARGET_VIEW_DESC rtvDesc = {};
+	// rtvDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM_SRGB;
+	// rtvDesc.ViewDimension = D3D11_RTV_DIMENSION_TEXTURE2D;
+	// g_pd3dDevice->CreateRenderTargetView(pBackBuffer, &rtvDesc, &g_mainRenderTargetView);
+	g_pd3dDevice->CreateRenderTargetView(pBackBuffer, nullptr, &g_mainRenderTargetView);
 	g_pd3dDevice->CreateUnorderedAccessView(pBackBuffer, NULL, &g_mainRenderTargetUav);
 	pBackBuffer->Release();
 }
